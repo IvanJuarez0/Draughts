ada8a670565e563e86ac3bc8f2ec0169e5b63826
Ivan Juarez <451701.clot@fje.edu>
2025-10-13 13:23:49 +0200

    Create and print board

--- Draughts.cpp
+++ Draughts.cpp
@@ -2,13 +2,47 @@
 //
 
 #include <iostream>
+#include <vector>
+
+std::vector<char> board{
+    '.', 'x', '.', 'x', '.', 'x', '.', 'x',
+    'x', '.', 'x', '.', 'x', '.', 'x', '.',
+    '.', 'x', '.', 'x', '.', 'x', '.', 'x',
+    '.', '.', '.', '.', '.', '.', '.', '.',
+    '.', '.', '.', '.', '.', '.', '.', '.',
+    'o', '.', 'o', '.', 'o', '.', 'o', '.',
+    '.', 'o', '.', 'o', '.', 'o', '.', 'o',
+    'o', '.', 'o', '.', 'o', '.', 'o', '.',
+};
+
 
 int main()
 {
-    std::cout << "Hello World!\n";
-    std::cout << "My name is Ivan J.!\n";
+    for (int i = 0; i < board.size(); ++i) {
+        if (i % 8 == 0)
+            std::cout << '\n';
+
+
+        std::cout << board.at(i);
+    }
+    std::cout << "\n\n";
 }
 
+    
+
+/*
+int main()
+{
+    std::cout << "Row 0: 0 1 2 3 4 5 6 7\n";
+    std::cout << "Row 1: 0 1 2 3 4 5 6 7\n";
+    std::cout << "Row 2: 0 1 2 3 4 5 6 7\n";
+    std::cout << "Row 3: 0 1 2 3 4 5 6 7\n";
+    std::cout << "Row 4: 0 1 2 3 4 5 6 7\n";
+    std::cout << "Row 5: 0 1 2 3 4 5 6 7\n";
+    std::cout << "Row 6: 0 1 2 3 4 5 6 7\n";
+    std::cout << "Row 7: 0 1 2 3 4 5 6 7\n";
+}
+*/
 // Ejecutar programa: Ctrl + F5 o menú Depurar > Iniciar sin depurar
 // Depurar programa: F5 o menú Depurar > Iniciar depuración
 
